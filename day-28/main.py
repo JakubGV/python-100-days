21eaf53574988f88cafeff3224cac2fed880160e
JakubGV <kubi.vogel7@hotmail.com>
2022-07-01 11:55:37 -0400

    Complete day 28, build pomodoro app

--- day-28/main.py
+++ day-28/main.py
@@ -8,21 +8,53 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    header_label.config(text="Timer", fg=GREEN)
+    canvas.itemconfig(timer_text, text="00:00")
+    checks_label['text'] = ""
+    global reps
+    reps = 0
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps %  8 == 0:
+        header_label.config(text="Break", fg=RED)
+        count_down(long_break_sec)
+    elif reps % 2 == 1:
+        header_label.config(text="Work", fg=GREEN)
+        count_down(work_sec)
+    else:
+        header_label.config(text="Break", fg=PINK)
+        count_down(short_break_sec)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
     count_min = count // 60 # Floor division
     count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
     
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        global reps
+        checks_label['text'] = "✔" * (reps // 2) # Put a checkmark for every 2 reps completed
+        start_timer()
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -41,10 +73,10 @@ canvas.grid(row=1 , column=1)
 start_btn = Button(text="Start", highlightthickness=0, command=start_timer)
 start_btn.grid(row=2, column=0)
 
-reset_btn = Button(text="Reset", highlightthickness=0)
+reset_btn = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset_btn.grid(row=2, column=3)
 
-checks_label = Label(text="✔", font=(FONT_NAME, 40, "bold"), fg=GREEN, bg=YELLOW)
+checks_label = Label(font=(FONT_NAME, 28, "bold"), fg=GREEN, bg=YELLOW)
 checks_label.grid(row=3, column=1)
 
 window.mainloop()
